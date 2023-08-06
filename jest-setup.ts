6af1f7ff928cbfde120776a5661680ea2dbde044
ahmed-5g <ahmed_5g@yahoo.com>
2023-08-07 02:48:20 +0300

    fix: fireEvent.scroll now wroking

--- jest-setup.ts
+++ jest-setup.ts
@@ -1,14 +1,15 @@
 require('react-native-reanimated').setUpTests();
 import '@testing-library/jest-native/extend-expect';
-import { ReactTestInstance } from 'react-test-renderer';
 
-// declare global {
-//   namespace jest {
-//     interface Matchers<R> extends Matchers<void, ReactTestInstance> {
-//       toHaveAnimatedStyle(
-//         style: Record<string, unknown> | Record<string, unknown>[],
-//         config?: { shouldMatchAllProps?: boolean }
-//       ): R;
-//     }
-//   }
-// }
+declare global {
+  namespace jest {
+    interface Matchers<R> {
+      toHaveAnimatedStyle(
+        style: Record<string, unknown>[] | Record<string, unknown>,
+        config?: {
+          shouldMatchAllProps?: boolean;
+        }
+      ): R;
+    }
+  }
+}
--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -1,25 +1,6 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react';
-import {
-  fireEvent,
-  render,
-  waitForElementToBeRemoved,
-} from '@testing-library/react-native';
+import { fireEvent, render } from '@testing-library/react-native';
 import { FlatListWithRectangleIndicator } from '../index';
-import {
-  NativeModules,
-  StyleProp,
-  ViewStyle,
-  findNodeHandle,
-} from 'react-native';
-
-describe('setPackages', () => {
-  // ...
-
-  test('example test', () => {
-    expect(true).toBe(true);
-  });
-});
 
 describe('FlatListWithRectangleIndicator', () => {
   const activeColor = 'rgba(0, 0, 0, 1)';
@@ -29,27 +10,26 @@ describe('FlatListWithRectangleIndicator', () => {
   const cardHeight = 200;
 
   const data = ['Item 1', 'Item 2', 'Item 3'];
-  const cardWidthPlusMarginValue = 100;
-  // const window = 100;
+  const cardWidthPlusMarginValue = 300;
 
-  // it('renders the flat list with correct number of items', () => {
-  //   const { getByTestId } = render(
-  //     <FlatListWithRectangleIndicator
-  //       data={data.slice(0, 3)}
-  //       cardWidthPlusMarginValue={cardWidthPlusMarginValue}
-  //       renderItem={undefined}
-  //       activeIndicatorColor={activeColor}
-  //       inActiveIndicatorColor={inactiveColor}
-  //     />
-  //   );
-  //   const indicator1 = getByTestId('indicator-0');
-  //   const indicator2 = getByTestId('indicator-1');
-  //   const indicator3 = getByTestId('indicator-2');
+  it('renders the flat list with correct number of items', () => {
+    const { getByTestId } = render(
+      <FlatListWithRectangleIndicator
+        data={data.slice(0, 3)}
+        cardWidthPlusMarginValue={cardWidthPlusMarginValue}
+        renderItem={undefined}
+        activeIndicatorColor={activeColor}
+        inActiveIndicatorColor={inactiveColor}
+      />
+    );
+    const indicator1 = getByTestId('indicator-0');
+    const indicator2 = getByTestId('indicator-1');
+    const indicator3 = getByTestId('indicator-2');
 
-  //   expect(indicator1).toBeTruthy();
-  //   expect(indicator2).toBeTruthy();
-  //   expect(indicator3).toBeTruthy();
-  // });
+    expect(indicator1).toBeTruthy();
+    expect(indicator2).toBeTruthy();
+    expect(indicator3).toBeTruthy();
+  });
 
   const activeStyle = {
     width: activeWidthValue,
@@ -59,7 +39,7 @@ describe('FlatListWithRectangleIndicator', () => {
     width: inActiveWidthValue,
     backgroundColor: inactiveColor,
   };
-  it('changes the width and background color of the indicators when scrolling', () => {
+  it('changes the width and background color of the indicators when scrolling', async () => {
     const { getByTestId } = render(
       <FlatListWithRectangleIndicator
         data={data}
@@ -74,31 +54,51 @@ describe('FlatListWithRectangleIndicator', () => {
     const indicator2 = getByTestId('indicator-1');
     const indicator3 = getByTestId('indicator-2');
 
-    // expect(indicator1).toHaveStyle(activeStyle);
-    // expect(indicator2).toHaveStyle(inActiveStyle);
-    // expect(indicator3).toHaveStyle(inActiveStyle);
+    expect(indicator1).toHaveStyle(activeStyle);
+    expect(indicator2).toHaveStyle(inActiveStyle);
+    expect(indicator3).toHaveStyle(inActiveStyle);
 
     const scrollableComponent = getByTestId('scrollable-component');
 
-    const eventData = {
-      nativeEvent: {
-        contentOffset: {
-          y: 0,
-          x: cardWidthPlusMarginValue,
+    function eventData(scrollXValue: number) {
+      return {
+        nativeEvent: {
+          contentOffset: {
+            y: 0,
+            //play around this value to show the live change with sharedValue change from 0 - cardWidthPlusMarginValue
+            x: scrollXValue,
+          },
+          contentSize: {
+            // Dimensions of the scrollable content
+            height: cardHeight,
+            width: cardWidthPlusMarginValue * data.length,
+          },
+          layoutMeasurement: {
+            // Dimensions of the device
+            height: 800,
+            width: 375,
+          },
         },
-        contentSize: {
-          // Dimensions of the scrollable content
-          height: cardHeight,
-          width: cardWidthPlusMarginValue * data.length,
-        },
-        layoutMeasurement: {
-          // Dimensions of the device
-          height: 800,
-          width: 375,
-        },
-      },
-    };
+      };
+    }
 
-    fireEvent.scroll(scrollableComponent, eventData);
+    // first scroll to the left with cardWidthPlusMarginValue
+    fireEvent.scroll(scrollableComponent, eventData(cardWidthPlusMarginValue));
+
+    jest.advanceTimersByTime(1);
+    expect(indicator1).toHaveAnimatedStyle(inActiveStyle);
+    expect(indicator2).toHaveAnimatedStyle(activeStyle);
+    expect(indicator3).toHaveAnimatedStyle(inActiveStyle);
+    //بالعربي
+    //second scroll to the right with cardWidthPlusMarginValue * 2
+    fireEvent.scroll(
+      scrollableComponent,
+      eventData(cardWidthPlusMarginValue * 2)
+    );
+
+    jest.advanceTimersByTime(1);
+    expect(indicator1).toHaveAnimatedStyle(inActiveStyle);
+    expect(indicator2).toHaveAnimatedStyle(inActiveStyle);
+    expect(indicator3).toHaveAnimatedStyle(activeStyle);
   });
 });
